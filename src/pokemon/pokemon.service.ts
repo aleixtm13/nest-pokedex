fc8979742806b15fc314cedc945c4e4260450151
aleixtm13 <aleix13t13m@gmail.com>
2024-10-07 16:03:19 +0200

    implement: pagination on find all pokemon

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -4,6 +4,7 @@ import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { isValidObjectId, Model } from 'mongoose';
 import { Pokemon } from './entities/pokemon.entity';
 import { InjectModel } from '@nestjs/mongoose';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Injectable()
 export class PokemonService {
@@ -27,8 +28,9 @@ export class PokemonService {
     return pokemon;
   }
 
-  findAll() {
-    return this.pokemonModel.find();
+  findAll(queryParameters: PaginationDto) {
+    const {limit  = 10, offset = 0} = queryParameters;
+    return this.pokemonModel.find().limit(limit).skip(offset).sort({no: 1});
   }
 
   async findOne(searchTerm: string) {
