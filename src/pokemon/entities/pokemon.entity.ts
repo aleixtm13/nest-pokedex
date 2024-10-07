2153d7c1ce5b6a0ec56e1560da8682b38ce33a02
aleixtm13 <aleix13t13m@gmail.com>
2024-10-07 10:50:18 +0200

    add: pokemon entity

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,18 @@
-export class Pokemon {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+@Schema()
+export class Pokemon extends Document {
+    @Prop({
+        unique: true,
+        idex: true,
+    })
+    name: string;
+    @Prop({
+        unique: true,
+        idex: true,
+    })
+    no: number;
+}
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
